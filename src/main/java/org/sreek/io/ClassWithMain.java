4bbc9c6c2e842854c436b52aff7648365eed55b9
Sreekar Swarnapuri <sreekar.swarnapuri@outlook.com>
2023-12-14 17:01:10 +0530

    Added mechanism to read from a file as well.

--- src/main/java/org/sreek/io/ClassWithMain.java
+++ src/main/java/org/sreek/io/ClassWithMain.java
@@ -5,7 +5,16 @@ public class ClassWithMain {
     public static void main(String[] args) {
         FileIO fileIO = new FileIO();
 
-        fileIO.WriteToFile("text.txt");
+        System.out.println("First file - text.txt - ");
+        fileIO.writeToFile("text.txt");
+
+        fileIO.readFromFile("text.txt");
+
+        System.out.println("Second file - text1.txt - ");
+        fileIO.writeToFile("text1.txt");
+
+        fileIO.readFromFile("text1.txt");
+
     }
 
 }
--- src/main/java/org/sreek/io/FileIO.java
+++ src/main/java/org/sreek/io/FileIO.java
@@ -1,13 +1,10 @@
 package org.sreek.io;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class FileIO {
-    
-    public void WriteToFile(String filePath) {
+
+    public void writeToFile(String filePath) {
         File file = new File(filePath);
         try (FileWriter fileWriter = new FileWriter(file); BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
 
@@ -18,7 +15,29 @@ public class FileIO {
             bufferedWriter.write("Third Line");
 
         } catch (IOException e) {
-            System.out.println("IOException occurred - " + e.getMessage());
+            System.out.println("IOException occurred while writing to file - " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void readFromFile(String filePath) {
+        File file = new File(filePath);
+
+        StringBuilder stringBuffer = new StringBuilder();
+
+        try(FileReader fileReader = new FileReader(file); BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+                stringBuffer.append(System.lineSeparator());
+            }
+
+            System.out.println("Contents of the file are: ");
+            System.out.println(stringBuffer);
+
+        } catch (IOException e) {
+            System.out.println("IOException occurred while reading from file - " + e.getMessage());
             e.printStackTrace();
         }
     }
