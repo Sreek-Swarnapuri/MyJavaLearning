887cd4822dc87a04118deaf14db8931095bb40dc
Sreekar Swarnapuri <sreekar.swarnapuri@outlook.com>
2023-12-25 13:54:25 +0530

    Using different primitive data types

---
+++ src/main/java/org/sreek/datatypes/MainClass.java
@@ -0,0 +1,37 @@
+package org.sreek.datatypes;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        // NUMERIC data types
+        // ------------------
+
+        // 8-bit signed integer - byte : -128 to 127
+        byte byt = -128;
+        System.out.println(byt);
+
+        // 16-bit signed integer - short : -32,768 to 32,767
+        short shrt = 32767;
+        System.out.println(shrt);
+
+        // 32-bit signed integer int: -2^31 to 2^31-1
+        int a =  (int) Math.pow(2,30);
+        System.out.println(a);
+
+        // 64-bit signed integer - long : -2^63 to 2^63-1
+        long l1 = 2389749827947328l;
+        System.out.println(l1);
+        long l2 = 983475749567348568L;
+        System.out.println(l2);
+
+        // floating point integer that store
+
+
+        // NON-NUMBERIC data types
+        // -----------------------
+
+
+    }
+
+}
