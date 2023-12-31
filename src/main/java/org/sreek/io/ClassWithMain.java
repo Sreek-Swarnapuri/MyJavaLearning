07db8cf3b589b64a35a6b62d95fbebeb8123645e
Sreekar Swarnapuri <sreekar.swarnapuri@outlook.com>
2024-01-01 00:16:14 +0530

    Adding loggers instead of sysout

--- src/main/java/org/sreek/io/ClassWithMain.java
+++ src/main/java/org/sreek/io/ClassWithMain.java
@@ -1,16 +1,21 @@
 package org.sreek.io;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ClassWithMain {
+
+    private static final Logger logger = LogManager.getLogger(ClassWithMain.class);
 
     public static void main(String[] args) {
         FileIO fileIO = new FileIO();
 
-        System.out.println("First file - text.txt - ");
+        logger.info("First file - text.txt - ");
         fileIO.writeToFile("text.txt");
 
-        fileIO.readFromFile("text.txt");
+        logger.info("text.txt");
 
-        System.out.println("Second file - text1.txt - ");
+        logger.info("Second file - text1.txt - ");
         fileIO.writeToFile("text1.txt");
 
         fileIO.readFromFile("text1.txt");
